e66c31f205bfb80f9441c27770151c5b063ddfdb
AtanasNikolov1 <atanasnik989@gmail.com>
2025-02-08 11:39:33 +0200

    Implement authService with signUpUser function

---
+++ src/services/authService.ts
@@ -0,0 +1,36 @@
+import { createUserWithEmailAndPassword, deleteUser } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { createUserProfile } from "./userService";
+import { handleError } from "../utils/errorHandler";
+
+export const signUpUser = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+) => {
+  let userCredential;
+
+  try {
+    userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const { uid } = userCredential.user;
+
+    await createUserProfile(uid, firstName, lastName, email);
+    return userCredential.user;
+  } catch (error) {
+    if (userCredential) {
+      try {
+        await deleteUser(userCredential.user);
+      } catch (deleteError) {
+        console.error("Error rolling back Firebase Auth user:", deleteError);
+      }
+    }
+
+    handleError(error, "Error during user sign-up or profile creation");
+  }
+};
