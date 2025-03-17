0b7754c8455afdd5ee6777ccbf43e2593bd0f67f
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-17 20:37:09 +0200

    feat(hooks): Create useAuth hook for Firebase authentication state

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,21 @@
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { useEffect, useState } from "react";
+
+const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+};
+
+export default useAuth;
