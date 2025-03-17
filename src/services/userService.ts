4e31341ed90437058056a0c0d20351ed5eaf7dde
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-17 20:51:20 +0200

    refactor(userService): Update createUserProfile to use fullName

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,18 +4,16 @@ import { handleError } from "../utils/errorHandler";
 
 export const createUserProfile = async (
   uid: string,
-  firstName: string,
-  lastName: string,
+  fullName: string,
   email: string
 ) => {
   try {
     await setDoc(doc(db, "users", uid), {
       createdAt: new Date(),
       email,
-      firstName,
-      lastName,
+      fullName,
     });
   } catch (error) {
-    handleError(error, "Error creating the user profile in Firestore");
+    handleError(error, "Failed to create the user profile in Firestore");
   }
 };
