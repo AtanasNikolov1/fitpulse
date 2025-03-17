83e95ea19dbc42beff31fd1f58dfaabafcd3071b
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-17 20:33:29 +0200

    refactor(pages): Refactor SignUpPage and LoginPage to use smaller reusable components

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,24 @@
-import LoginForm from "../components/features/auth/LoginForm";
+import AuthFormWrapper from "../components/features/auth/ui/AuthFormWrapper";
+import AuthRedirectLink from "../components/features/auth/ui/AuthRedirectLink";
+import AuthTitle from "../components/features/auth/ui/AuthTitle";
+import AuthWallpaper from "../components/features/auth/ui/AuthWallpaper";
+import AuthDivider from "../components/features/auth/ui/AuthDivider";
+import LoginForm from "../components/features/auth/forms/LoginForm";
+import GoogleSignInButton from "../components/features/auth/ui/GoogleSignInButton ";
 
-const Login = () => {
-  return <LoginForm />;
+const LoginPage = () => {
+  return (
+    <div className="flex min-h-screen">
+      <AuthWallpaper />
+      <AuthFormWrapper>
+        <AuthTitle text="Welcome Back to FitPulse" />
+        <LoginForm />
+        <AuthRedirectLink text="No account yet?" url="/signup" />
+        <AuthDivider />
+        <GoogleSignInButton />
+      </AuthFormWrapper>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,24 @@
-import SignUpForm from "../components/features/auth/SignupForm";
+import AuthFormWrapper from "../components/features/auth/ui/AuthFormWrapper";
+import AuthTitle from "../components/features/auth/ui/AuthTitle";
+import AuthWallpaper from "../components/features/auth/ui/AuthWallpaper";
+import AuthDivider from "../components/features/auth/ui/AuthDivider";
+import AuthRedirect from "../components/features/auth/ui/AuthRedirectLink";
+import GoogleSignInButton from "../components/features/auth/ui/GoogleSignInButton ";
+import SignUpForm from "./../components/features/auth/forms/SignUpForm";
 
 const SignUpPage = () => {
-  return <SignUpForm />;
+  return (
+    <div className="flex min-h-screen">
+      <AuthWallpaper />
+      <AuthFormWrapper>
+        <AuthTitle text="Start Your Journey with Fitpulse" />
+        <SignUpForm />
+        <AuthRedirect text="Already have an account?" url="/login" />
+        <AuthDivider />
+        <GoogleSignInButton />
+      </AuthFormWrapper>
+    </div>
+  );
 };
 
 export default SignUpPage;
