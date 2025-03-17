c98970f0a5153e8c7ae774dc67a4a8c1f16f1636
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-17 20:10:07 +0200

    feat: Integrate react-toastify for notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes/router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <ToastContainer position="top-right" />
     </>
   );
 }
