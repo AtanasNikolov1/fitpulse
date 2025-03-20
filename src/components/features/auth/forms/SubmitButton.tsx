d5b635598849e75df6ba9922dde0c6390efca26b
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-20 17:57:58 +0200

    style(auth): Improve spacing and transitions for better UI

--- src/components/features/auth/forms/SubmitButton.tsx
+++ src/components/features/auth/forms/SubmitButton.tsx
@@ -6,7 +6,7 @@ const SubmitButton = ({ text }: SubmitButtonProps) => {
   return (
     <button
       type="submit"
-      className="bg-soft-violet text-pure-white py-3 px-6 sm:px-8 border border-soft-violet rounded-lg text-sm sm:text-base md:text-lg font-semibold hover:bg-deep-violet hover:cursor-pointer hover:border-deep-violet mt-6 sm:mt-8 w-full sm:w-auto"
+      className="bg-soft-violet text-pure-white py-3 px-6 sm:px-8 border border-soft-violet rounded-lg text-sm sm:text-base md:text-lg font-semibold hover:bg-deep-violet hover:cursor-pointer hover:border-deep-violet mt-6 sm:mt-8 w-full sm:w-auto transition"
     >
       {text}
     </button>
--- src/components/features/auth/ui/AuthFormWrapper.tsx
+++ src/components/features/auth/ui/AuthFormWrapper.tsx
@@ -6,7 +6,7 @@ type AuthFormWrapperProps = {
 
 const AuthFormWrapper = ({ children }: AuthFormWrapperProps) => {
   return (
-    <section className="flex flex-col gap-4 justify-center items-center w-full max-w-md mx-auto px-10 my-8">
+    <section className="flex flex-col gap-4 justify-center items-center w-full max-w-md mx-auto px-10 mb-10 mt-30">
       {children}
     </section>
   );
