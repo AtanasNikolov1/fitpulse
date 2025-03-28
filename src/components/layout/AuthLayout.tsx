46b954d8a180e3a6fb1ca9bf7b246aa1da27d3bc
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-28 12:58:54 +0200

    feat(layout): add ScrollToTop component for route change scroll reset

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./navigation/NavBar";
+import ScrollToTop from "../../utils/ScrollToTop";
 
 const AuthLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBar />
       <Outlet />
     </>
--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./navigation/NavBar";
 import Footer from "./footer/Footer";
+import ScrollToTop from "../../utils/ScrollToTop";
 
 const PublicLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBar />
       <Outlet />
       <Footer />
