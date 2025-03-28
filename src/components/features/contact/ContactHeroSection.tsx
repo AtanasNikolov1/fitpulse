a034869923601cd7d2565482f6e75d4b665b0e42
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-28 10:38:47 +0200

    feat(contact): add ContactHeroSection with title and subtitle

---
+++ src/components/features/contact/ContactHeroSection.tsx
@@ -0,0 +1,15 @@
+const ContactHeroSection = () => {
+  return (
+    <section className="px-6 py-12 lg:py-15 mt-17  bg-gradient-to-r from-soft-violet to-deep-violet text-white  text-center">
+      <h1 className="mb-4 text-3xl sm:text-4xl lg:text-5xl font-extrabold">
+        We're Here to Help
+      </h1>
+      <p className="text-base sm:text-lg lg:text-xl">
+        Reach out with any questions, feedback, or support requests—we’d love to
+        hear from you!
+      </p>
+    </section>
+  );
+};
+
+export default ContactHeroSection;
