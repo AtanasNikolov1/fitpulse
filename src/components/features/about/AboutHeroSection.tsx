822d3b5ae4a0e6271891e8c30f5f149ea8d92c64
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-28 10:45:28 +0200

    style(about): change gradient color in AboutHeroSection

--- src/components/features/about/AboutHeroSection.tsx
+++ src/components/features/about/AboutHeroSection.tsx
@@ -1,6 +1,6 @@
 const AboutHeroSection = () => {
   return (
-    <section className="px-6 py-12 lg:py-15 mt-17  bg-gradient-to-r from-indigo-500 to-purple-600 text-white  text-center">
+    <section className="px-6 py-12 lg:py-15 mt-17  bg-gradient-to-r from-soft-violet to-deep-violet text-white  text-center">
       <h1 className="mb-4 text-3xl sm:text-4xl lg:text-5xl font-extrabold">
         About FitPulse
       </h1>
