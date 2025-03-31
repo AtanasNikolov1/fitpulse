01d85b81b4cd2f76aee6cbbff904d9f81f3aa099
AtanasNikolov1 <atanasnik989@gmail.com>
2025-03-31 19:32:30 +0300

    feat(layout/sidebar): add ExpandedSidebar for full navigation experience

---
+++ src/components/layout/sidebar/ExpandedSidebar.tsx
@@ -0,0 +1,69 @@
+import { NavLink } from "react-router-dom";
+import { ChevronLeft, LogOut } from "lucide-react";
+import { menuItems } from "./sidebarMenuData";
+
+type ExtendedSidebarProps = {
+  toggleSidebar: () => void;
+};
+
+const ExpandedSidebar = ({ toggleSidebar }: ExtendedSidebarProps) => {
+  const baseLinkClasses =
+    "flex items-center gap-5 px-4 py-3 w-full text-pure-white font-semibold rounded-xl transition-colors cursor-pointer";
+  const hoverClasses = "hover:bg-soft-violet hover:text-pure-white";
+  const activeClasses =
+    "bg-soft-violet text-white shadow-md shadow-deep-violet";
+
+  return (
+    <nav className="flex flex-col justify-between w-68 h-screen md:sticky md:top-0 md:left-0 lg:relative md:z-50 lg:z-0 py-3 bg-sidebar transition-width duration-300">
+      {/* Sidebar wrapper for the header and the navigation links */}
+      <div className="flex flex-col gap-14 w-full">
+        {/* Sidebar Header  */}
+        <div className="flex justify-between items-center min-h-16 w-full p-4 text-white">
+          <img
+            src="/images/fitpulse-sidebar-logo.png"
+            alt="Fitpulse logo"
+            className="h-8"
+          />
+          <div
+            className={`p-1.5 rounded-xl ${hoverClasses} cursor-pointer`}
+            onClick={toggleSidebar}
+          >
+            <ChevronLeft size={28} />
+          </div>
+        </div>
+        {/* Navigation Links */}
+        <div className="flex flex-col justify-center items-start gap-8 w-full px-4 ">
+          {menuItems.map((item, index) => {
+            return (
+              <NavLink
+                key={index}
+                to={item.link}
+                className={({ isActive }) =>
+                  `${baseLinkClasses} ${
+                    isActive ? activeClasses : hoverClasses
+                  }`
+                }
+              >
+                <item.icon size={28} />
+                <p className="text-lg">{item.name}</p>
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* Logout button */}
+      <div className="w-full px-4 mb-6">
+        <button
+          className={`${baseLinkClasses} bg-[#454545] ${hoverClasses} cursor-pointer`}
+          type="button"
+        >
+          <LogOut size={28} className="ml-2" />
+          <p className="text-lg ml-2">Log Out</p>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default ExpandedSidebar;
