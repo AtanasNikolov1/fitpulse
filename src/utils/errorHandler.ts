b88253cd007846dc1df09f0c4d548651f710d566
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-06 18:32:22 +0300

    feat(utils): add createCustomErrorMessage for centralized error handling

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,11 +1,24 @@
 import { FirebaseError } from "firebase/app";
 
-export const handleError = (error: unknown, customMessage: string) => {
+export const handleError = (error: unknown, customMessage: string): never => {
   if (error instanceof FirebaseError) {
     console.error(`${customMessage}: ${error.message}`);
     throw new Error(`An error occurred: ${customMessage}`);
   } else {
     console.error(`Unexpected error: ${error}`);
     throw new Error(`An unexpected error occurred: ${error}`);
+  }
+};
+
+export const createCustomErrorMessage = (
+  error: unknown,
+  customMessage: string
+): string => {
+  if (error instanceof FirebaseError) {
+    console.error(`${customMessage}: ${error.message}`);
+    return `An error occurred: ${customMessage}`;
+  } else {
+    console.error(`Unexpected error: ${error}`);
+    return `An unexpected error occurred: ${error}`;
   }
 };
