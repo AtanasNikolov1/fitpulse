5eea2725b7dc3163ba86945a8f224f37e7ed1b64
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-06 18:51:26 +0300

    feat(hooks): create custom hook to retrieve logged-in user ID

---
+++ src/hooks/useUserId.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useUserId = (): string | null => {
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserId(user.uid);
+      } else {
+        setUserId(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return userId;
+};
