eed9d30a024864e45d109438336fb8acf7b14b24
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-06 21:19:30 +0300

    feat(progress): add hero section for progress tracking page

---
+++ src/components/features/progress/ProgressHeroSection.tsx
@@ -0,0 +1,15 @@
+const ProgressHeroSection = () => {
+  return (
+    <section className="px-6 py-8 lg:py-10 bg-gradient-to-r from-soft-violet to-deep-violet text-white  text-center">
+      <h1 className="mb-4 text-2xl sm:text-3xl lg:text-4xl font-extrabold">
+        Track Your Progress
+      </h1>
+      <p className="text-base lg:text-xl">
+        Visualize your fitness journey with insights into your weight, steps,
+        sleep, and hydration.
+      </p>
+    </section>
+  );
+};
+
+export default ProgressHeroSection;
