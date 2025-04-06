d1e5ee97990a73ccba9a33327e826bb05ad1fcf8
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-06 19:37:20 +0300

    feat(types): create weight-related types for tracking and mutation

---
+++ src/types/weightTypes.ts
@@ -0,0 +1,17 @@
+import { Records } from "./recordTypes";
+
+export type WeightRecord = {
+  createdAt: string;
+  weight: number;
+};
+
+export type WeightRecordWithId = WeightRecord & {
+  id: string;
+};
+
+export type WeightMutationVariables = {
+  userId: string;
+  weight: number;
+};
+
+export type WeightRecords = Records<WeightRecord>;
