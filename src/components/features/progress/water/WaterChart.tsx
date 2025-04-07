2af90e686cf0b9f6e9b121370433144ca3fabe3b
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-07 22:48:11 +0300

    feat(progress/water): add WaterChart component for visualizing water amount data

---
+++ src/components/features/progress/water/WaterChart.tsx
@@ -0,0 +1,43 @@
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ReferenceLine,
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+} from "recharts";
+import { WaterRecords } from "../../../../types/waterTypes";
+
+type WaterChartProps = {
+  data: WaterRecords;
+};
+
+const WaterChart = ({ data }: WaterChartProps) => {
+  return (
+    <ResponsiveContainer width="100%" height={250}>
+      <AreaChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="createdAt" />
+        <YAxis domain={[0, 4000]} />
+        <Tooltip />
+        <ReferenceLine
+          y={2000}
+          stroke="#FE0000"
+          strokeDasharray="3 3"
+          label="Target (2000ml)"
+        />
+        <Area
+          type="monotone"
+          dataKey="amount"
+          stroke="#7636dd"
+          fill="#7636dd"
+          fillOpacity={0.3}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default WaterChart;
