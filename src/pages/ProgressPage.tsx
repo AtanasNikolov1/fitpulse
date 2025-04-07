8239da8bcabedc7a32fa939456168375efe22f7e
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-07 22:57:07 +0300

    chore(pages): remove unused  import from ProgressPage

--- src/pages/ProgressPage.tsx
+++ src/pages/ProgressPage.tsx
@@ -1,10 +1,8 @@
 import ProgressHeroSection from "../components/features/progress/ProgressHeroSection";
+import WeightTracker from "../components/features/progress/weight/WeightTracker";
 import SleepTracker from "../components/features/progress/sleep/SleepTracker";
-import SleepChart from "../components/features/progress/SleepChart";
 import StepsTracker from "../components/features/progress/steps/StepsTracker";
 import WaterTracker from "../components/features/progress/water/WaterTracker";
-
-import WeightTracker from "../components/features/progress/weight/WeightTracker";
 
 const ProgressPage = () => {
   return (
