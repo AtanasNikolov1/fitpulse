a783e23f91313848477125a521ac4e005d0b875e
AtanasNikolov1 <atanasnik989@gmail.com>
2025-04-07 22:51:25 +0300

    feat(progress/water): add WaterTracker component for displaying water intake chart and daily data

---
+++ src/components/features/progress/water/WaterTracker.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import { useUserId } from "../../../../hooks/useUserId";
+import {
+  getTodayWater,
+  getWaterRecords,
+} from "../../../../services/waterService";
+import { WaterRecords, WaterRecordWithId } from "../../../../types/waterTypes";
+import { transformRecordDates } from "../../../../utils/formatData";
+import TrackerWrapper from "../TrackerWrapper";
+import WaterChart from "./WaterChart";
+import DailyWater from "./DailyWater";
+
+const WaterTracker = () => {
+  const userId = useUserId();
+
+  const {
+    data: chartData,
+    isLoading: isChartLoading,
+    error: chartError,
+  } = useQuery<WaterRecords, Error>({
+    queryKey: ["water", userId],
+    queryFn: () => getWaterRecords(userId!),
+    select: (data) => transformRecordDates(data),
+  });
+
+  const {
+    data: dailyWater,
+    isLoading: isDailyLoading,
+    error: dailyError,
+  } = useQuery<WaterRecordWithId | null, Error>({
+    queryKey: ["waterToday", userId],
+    queryFn: () => getTodayWater(userId!),
+  });
+
+  if (isChartLoading || isDailyLoading) return <div>Loading...</div>;
+  if (chartError || dailyError) return <div>Error loading water data</div>;
+
+  return (
+    <TrackerWrapper>
+      <WaterChart
+        data={
+          chartData && chartData.length > 0
+            ? chartData
+            : [{ createdAt: "No Records", amount: 0 }]
+        }
+      />
+      <DailyWater data={dailyWater ?? null} />
+    </TrackerWrapper>
+  );
+};
+
+export default WaterTracker;
